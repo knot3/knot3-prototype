a904cf108e309c1ec306a78479a6bc55012156c5
Tobias Schulz <tobias@freehal.net>
2013-12-05 11:05:47 +0100

    added C# parser and UML graph generator

--- KnotTest/Knot3/Knot3/Settings/Options.cs
+++ KnotTest/Knot3/Knot3/Settings/Options.cs
@@ -120,10 +120,14 @@ namespace Knot3.Settings
 
 	public class BooleanOptionInfo : DistinctOptionInfo
 	{
+		public new static string[] ValidValues = new string[] {
+			ConfigFile.True,
+			ConfigFile.False
+		};
+
 		public BooleanOptionInfo (string section, string name, bool defaultValue, Action<bool> onChange = null,
 		                          ConfigFile configFile = null)
-			: base(section, name, defaultValue?ConfigFile.True:ConfigFile.False,
-			       new string[]{ConfigFile.True,ConfigFile.False}, (str)=>{}, configFile)
+			: base(section, name, defaultValue?ConfigFile.True:ConfigFile.False, ValidValues, null, configFile)
 		{
 			if (onChange != null) {
 				OnChange = (str) => onChange (str == ConfigFile.True);
